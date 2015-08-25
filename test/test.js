21083abbdc4544c6049dda6585ddc4194478ba1b
Yevgeny Petukhov <petukhov.yevgeny@gmail.com>
2015-08-25 23:29:07 +0300

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,31 @@
+var postxml = require('../');
+var expect = require('chai').expect;
+
+var plugin1 = require('./plugin1.js');
+var plugin2 = require('./plugin2.js');
+
+var test = function (input, output, opts) {
+   //  var $ = cheerio.load(input);
+
+   //  plugin(opts)($);
+   var processed = postxml(
+      '<block class="b-block"><element class="b-block__element">Текст</element></block>',
+      {},
+      [
+         plugin1(['element']),
+         plugin2(['div'])
+      ]
+   );
+
+   expect(output).to.eql(processed);
+};
+
+describe('postxml', function () {
+    it('link tag', function () {
+        test(
+            '<block class="b-block"><element class="b-block__element">Текст</element></block>',
+            '<block class="b-block"><div class="b-block__element" mod="mod">Текст</div></block>',
+            {}
+        );
+    });
+});
