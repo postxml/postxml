985f7a46d8ecc8556f3bbe028ec83767f6e54d5b
alanev <e@alanev.ru>
2015-08-27 11:28:31 +0300

    Add examples in readme. Fixed options bugs and also tests.

--- test/test.js
+++ test/test.js
@@ -4,16 +4,13 @@ var expect = require('chai').expect;
 var plugin1 = require('./plugin1.js');
 var plugin2 = require('./plugin2.js');
 
-var test = function (input, output, opts) {
+var test = function (input, output, opts, plugins) {
    //  var $ = cheerio.load(input);
 
    //  plugin(opts)($);
    var processed = postxml(
       '<block class="b-block"><element class="b-block__element">Текст</element></block>',
-      [
-         plugin1(['element']),
-         plugin2(['div'])
-      ],
+      plugins,
       opts
    );
 
@@ -21,11 +18,15 @@ var test = function (input, output, opts) {
 };
 
 describe('postxml', function () {
-    it('link tag', function () {
+    it('change tag', function () {
         test(
             '<block class="b-block"><element class="b-block__element">Текст</element></block>',
             '<block class="b-block"><div class="b-block__element" mod="mod">Текст</div></block>',
-            {}
+            {},
+            [
+               plugin1(['element']),
+               plugin2(['div'])
+            ]
         );
     });
 });
