f3284da5a1714faf3cdb993a896b4ba2611b3f56
Yevgeny Petukhov <petukhov.yevgeny@gmail.com>
2015-08-29 12:52:54 +0300

    Fixed cheerioOptions bug

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var cheerio = require('cheerio');
 
 module.exports = function(html, plugins, opts){
    var options = {};
-   options.normalizeWhitespace = (opts.normalizeWhitespace != undefined) ? opts.normalizeWhitespace : true;
-   options.xmlMode = (opts.xmlMode != undefined) ? opts.xmlMode : false;
-   options.decodeEntities = (opts.decodeEntities != undefined) ? opts.decodeEntities : false;
+   if( opts != undefined ){
+      options.normalizeWhitespace = (opts.normalizeWhitespace != undefined) ? opts.normalizeWhitespace : true;
+      options.xmlMode = (opts.xmlMode != undefined) ? opts.xmlMode : false;
+      options.decodeEntities = (opts.decodeEntities != undefined) ? opts.decodeEntities : false;
+   }
 
    // load
    var $ = cheerio.load(html, options);
