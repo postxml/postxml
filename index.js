a42bd2bb7ed2fad39dddeaa840498c2eed717f5d
alanev <e@alanev.ru>
2015-08-30 15:04:57 +0300

    Fixed options bug

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 var cheerio = require('cheerio');
 
+var set = function (variable, alt) {
+    return (variable != undefined) ? variable : alt;
+};
+
 module.exports = function(html, plugins, opts){
-   var options = {};
-   if( opts != undefined ){
-      options.normalizeWhitespace = (opts.normalizeWhitespace != undefined) ? opts.normalizeWhitespace : true;
-      options.xmlMode = (opts.xmlMode != undefined) ? opts.xmlMode : false;
-      options.decodeEntities = (opts.decodeEntities != undefined) ? opts.decodeEntities : false;
-   }
+    var options = {};
+    options.normalizeWhitespace = set(options.normalizeWhitespace, true);
+    options.xmlMode = set(options.xmlMode, false);
+    options.decodeEntities = set(options.decodeEntities, false);
 
-   // load
-   var $ = cheerio.load(html, options);
+    // load
+    var $ = cheerio.load(html, options);
 
-   // transform with plugins
-   for(var i = 0, len = plugins.length; i < len; i++){
-      plugins[i]($);
-   }
+    // transform with plugins
+    for(var i = 0, len = plugins.length; i < len; i++){
+        plugins[i]($);
+    }
 
-   return $.html();
+    return $.html();
 };
